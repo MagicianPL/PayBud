758868d8a30c514ad65b677e35edb7e52a0ee15b
Michał <sukowskimichal@gmail.com>
2022-05-07 15:48:23 +0200

    TopNavigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { ThemeProvider } from "styled-components";
 
 import theme from "./theme/theme";
+import TopNavigation from "./components/TopNavigation/TopNavigation";
 import Test from "./components/Test";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
+        <TopNavigation />
         <Test />
       </div>
     </ThemeProvider>
---
+++ src/components/TopNavigation/TopNavigation.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import StyledWrapper from './StyledWrapper';
+
+const TopNavigation = () => {
+    return(
+        <StyledWrapper>
+
+        </StyledWrapper>
+    );
+};
+
+export default TopNavigation;
