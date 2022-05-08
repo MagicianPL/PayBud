9acd3a9f6a3f99bbf7e691360bfe3596c3aeb0a4
Michał <sukowskimichal@gmail.com>
2022-05-09 01:16:29 +0200

    Homepage almost done

--- src/components/Homepage/InfoStep/InfoStep.tsx
+++ src/components/Homepage/InfoStep/InfoStep.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { GiReceiveMoney } from 'react-icons/gi';
+import { FaUserTie } from 'react-icons/fa';
 
 import StyledWrapper from './StyledWrapper';
+import StyledButton from '../../StyledButton/StyledButton';
 
 const InfoStep = () => {
     return(
         <StyledWrapper>
             <div className="flex">
             <div>
+                <GiReceiveMoney />
                 <p>Wpisujesz wszystkie dane oraz kwotę, prosto i szybko generujesz link do płatności, który przesyłasz klientowi.</p>
                 <p> Po opłaceniu dostajesz powiadomienie sms, a pieniądze trafiają na Twoje konto!</p>
                 <p>Przyjemnie, łatwo i intuicyjnie!</p>
@@ -16,12 +20,16 @@ const InfoStep = () => {
 
             <div className="flex">
             <div>
+                <FaUserTie />
                 <p>Paylink to także Twój osobisty Asystent Płatności!</p>
                 <p>Każdy wygenerowany link, to osobna transakcja, do której możesz przypiąć notatki oraz nimi zarządzać.</p>
                 <p>Twoja sprzedaż w jednym miejscu - Z Paylink to proste!</p>
             </div>
             <div></div>
             </div>
+            <p className="footer">Chcesz tworzyć notatki nie generując linków? Żaden problem!</p>
+            <p className="footer">W Paylink to Ty masz pełną kontrolę - możesz używać konta wyłącznie jako swój osobisty system zarządzania.</p>
+            <StyledButton>Zakładam konto</StyledButton>
         </StyledWrapper>
     );
 };
--- src/components/Homepage/InfoStep/StyledWrapper.tsx
+++ src/components/Homepage/InfoStep/StyledWrapper.tsx
@@ -2,26 +2,48 @@ import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
 
+p {
+    text-align: left;
+    margin-bottom: 20px;
+    font-size: 22px;
+    color: ${({theme}) => theme.colors.primary};
+}
+
     .flex {
         display: flex;
         justify-content: space-between;
         align-items: flex-start;
 
-        p {
-            text-align: left;
-            margin-bottom: 20px;
-            font-size: 22px;
-            color: ${({theme}) => theme.colors.primary};
+        div:nth-child(odd) {
+            position: relative;
+            bottom: 35px;
         }
     
         div:nth-child(even) {
             border: 3px solid purple;
             width: 100%;
-            max-width: 600px;
+            max-width: 550px;
             height: 300px;
             margin-left: 30px;
             margin-bottom: 45px;
         }
+    }
+
+    .flex:nth-of-type(2) {
+        margin-bottom: 55px;
+    }
+
+    svg {
+        font-size: 35px;
+    }
+
+    .footer {
+        text-align: center;
+        margin-bottom: 30px;
+    }
+
+    .footer:nth-child(even) {
+        margin-bottom: 50px;
     }
 `;
 
