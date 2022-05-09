dfecc8abc5100cab2c66d3731489617fcfe420d6
Michał <sukowskimichal@gmail.com>
2022-05-09 11:00:14 +0200

    Just a visible line under TopNavigation

--- src/components/TopNavigation/StyledWrapper.tsx
+++ src/components/TopNavigation/StyledWrapper.tsx
@@ -3,7 +3,19 @@ import styled from "styled-components";
 const StyledWrapper = styled.div`
     width: 100%;
     min-height: 50px;
-    background: lightgray;
+    position: relative;
+
+    &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 80%;
+        height: 1px;
+        background: ${({theme}) => theme.colors.secondary};
+        opacity: 0.1;
+    }
 
 `;
 
