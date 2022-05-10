558a5e574559f31f90b9e4b6c326ee1cd4037a0e
Michał <sukowskimichal@gmail.com>
2022-05-10 12:29:47 +0200

    React Router dependency

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <div className="App">
         <TopNavigation />
-        {/*<Homepage />*/}
+        <Homepage />
         <SignUpForm />
 
       </div>
