204bc575b9c72c832af8fb7442f2c6eacb02f05c
Michał <sukowskimichal@gmail.com>
2022-05-12 11:56:10 +0200

    Styles change

--- src/components/Modal/StyledModal.tsx
+++ src/components/Modal/StyledModal.tsx
@@ -1,16 +1,15 @@
 import styled from 'styled-components';
 
 const StyledModal = styled.div`
-    width: 650px;
-    max-width: 90%;
-    min-height: 100px;
+    width: 90%;
+    max-width: 650px;
+    min-height: 450px;
     background: white;
     border-radius: 6px;
     position: absolute;
     top: -20px;
     left: 50%;
     transform: translateX(-50%);
-    border: 1px solid ${({theme}) => theme.colors.primary};
     opacity: 0;
     animation: slideFromTop 1s forwards;
 
--- src/components/Modal/StyledOverlay.tsx
+++ src/components/Modal/StyledOverlay.tsx
@@ -6,7 +6,7 @@ const Overlay = styled.div`
     position: fixed;
     top: 0;
     left: 0;
-    background: rgba(0, 0, 0, 0.3);
+    background: rgba(0, 0, 0, 0.5);
     z-index: 20;
 `;
 
