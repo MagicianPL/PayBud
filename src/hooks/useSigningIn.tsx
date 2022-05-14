7fdcccba884860c7a32c612f50b176d566ef997e
Michał <sukowskimichal@gmail.com>
2022-05-14 09:35:33 +0200

    useSigningIn custom Hook

---
+++ src/hooks/useSigningIn.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from 'react-router-dom';
+
+import useModal from './useModal';
+
+const useSigningIn = () => {
+    const navigate = useNavigate();
+    const { showModal, setShowModal, modalError, setModalError } = useModal();
+
+    const signingIn = async (e: React.FormEvent<HTMLFormElement>, formValues: {
+        email: string
+        password: string
+    }) => {
+        e.preventDefault();
+        setShowModal(true);
+
+        //Simple, quick Validation
+        if (!formValues.email || !formValues.password) {
+            setModalError("Nieprawidłowa nazwa użytkownika lub hasło.");
+        };
+
+        //POST Request
+        const res = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formValues)
+        });
+        const data = await res.json();
+
+        if(!res.ok) return setModalError(data.message);
+
+        //Success
+        navigate('/account');
+        setShowModal(false);
+        setModalError("");
+    };
+
+    const onModalClick = () => {
+        setShowModal(false);
+        setModalError("");
+    };
+
+    return { signingIn, showModal, modalError, onModalClick };
+};
+
+export default useSigningIn;
